31a9399cd99e8ab0942d19ca11f73e5d65d3a8f6
sabrinothman95 <68713978+sabrinothman95@users.noreply.github.com>
2025-02-16 18:26:04 -0500

    added toml edited eslint, app succsessfu;lly deploys but app link is not working when i share it

--- my-portfolio/eslint.config.js
+++ my-portfolio/eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
 import globals from 'globals'
 import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
+// import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
